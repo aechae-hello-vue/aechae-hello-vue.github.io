24f22e6052c69db9026f4d3e70e0f856ed2f20ac
ae-chae <aechae11@gmail.com>
2024-01-31 11:39:27 +0900

    hello count + rev name

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
 <script setup>
-import { ref } form 'vue'
-const count = 
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref({
+	cnt: 0,
+	name: "YUN"
+})
+
+// functions that mutate state and trigger updates
+function increment() {
+count.value.cnt++
+count.value.name = count.value.name.split('').reverse().join('')
+}
+
+// lifecycle hooks
+onMounted(() => {
+	console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+	<button @click="increment">Count is: {{ count.cnt }} {{ count.name }}</button>
+</template>
+
+<style scoped>
+button {
+	font-weight: bold;
+}
+</style>
+ 
